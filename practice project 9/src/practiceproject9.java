9865fe02f5f78c65e7f7bfe050179d117e5c9f07
sudhirreddy1122 <sudhir17072000@gmail.com>
2022-07-23 10:41:06 +0530

    arrays

---
+++ practice project 9/src/practiceproject9.java
@@ -0,0 +1,16 @@
+
+public class practiceproject9 {
+
+public static void main(String[] args) {
+
+int a[]= {30,20,80,50,90};
+for(int i=0;i<5;i++) {
+System.out.println("Elements of array a: "+a[i]);
+}
+
+
+int[][] b = {{2, 4, 6, 8},{7, 5, 10} };
+      
+      System.out.println("\nLength of row 1: " + b[0].length);
+      }
+}
