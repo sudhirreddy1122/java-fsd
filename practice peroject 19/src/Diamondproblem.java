b07ede4842db8a89ea5d61f78b5cce298806b74b
sudhirreddy1122 <sudhir17072000@gmail.com>
2022-07-24 10:55:32 +0530

    Diamond problem

---
+++ practice peroject 19/src/Diamondproblem.java
@@ -0,0 +1,27 @@
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}  
+public class Diamondproblem implements First, Second 
+{  
+    public void show() 
+    {  
+        First.super.show(); 
+        Second.super.show(); 
+    } 
+    public static void main(String args[]) 
+    { 
+    	Diamondproblem ob = new Diamondproblem(); 
+        ob.show(); 
+    } 
+}
